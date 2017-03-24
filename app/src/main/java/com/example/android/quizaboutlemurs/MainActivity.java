b7110cdcd60c6a4574b1c62a71e90824b2cfc8d8
CannibalChoco <fudgeblimp@gmail.com>
2017-03-24 18:18:33 +0200

    All questions, except Q1,  evaluated. Quiz scored without Q1.

--- app/src/main/java/com/example/android/quizaboutlemurs/MainActivity.java
+++ app/src/main/java/com/example/android/quizaboutlemurs/MainActivity.java
@@ -11,55 +11,78 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     int score = 0;
-    // email
-
-
-    RadioButton radioButtonGambia;
+    //Q2
+    RadioButton radioButtonVatican;
     RadioButton radioButtonEurope;
     RadioButton radioButtonMadagascar;
     RadioButton radioButtonSriLanka;
 
-    // Q2
+    // Q3
     RadioButton radioButtonAncestorsYes;
     RadioButton radioButtonAncestorsNo;
 
-    // Q3
+    // Q4
     CheckBox checkbox30g;
     CheckBox checkbox9kg;
+    CheckBox checkbox50kg;
     CheckBox checkbox200kg;
 
-    // Q4
+    // Q5
     CheckBox checkboxInsects;
     CheckBox checkboxFruitAndVegetables;
     CheckBox checkboxPlants;
     CheckBox checkboxFish;
 
+    // Q6
+    RadioButton radioButtonVerySocial;
+    RadioButton radioButtonNotVerySocial;
+    RadioButton radioButtonNotSocial;
 
+    // Q7
+    RadioButton radioButton18;
+    RadioButton radioButton16;
+    RadioButton radioButton20;
+    RadioButton radioButton22;
+
+    // Q8
+    RadioButton radioButtonLemurYes;
+    RadioButton radioButtonLemurNo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Q1
-        radioButtonGambia = (RadioButton) findViewById(R.id.radio_gambia);
+        // Q2
+        radioButtonVatican = (RadioButton) findViewById(R.id.radio_gambia);
         radioButtonEurope = (RadioButton) findViewById(R.id.radio_europe);
         radioButtonMadagascar = (RadioButton) findViewById(R.id.radio_madagascar);
         radioButtonSriLanka = (RadioButton) findViewById(R.id.radio_sri_lanka);
-        // Q2
+        // Q3
         radioButtonAncestorsYes = (RadioButton) findViewById(R.id.radio_ancestors_yes);
         radioButtonAncestorsNo = (RadioButton) findViewById(R.id.radio_ancestors_no);
-        // Q3
+        // Q4
         checkbox30g = (CheckBox) findViewById(R.id.checkbox_30g);
         checkbox9kg = (CheckBox) findViewById(R.id.checkbox_9kg);
+        checkbox50kg = (CheckBox) findViewById(R.id.checkbox_50kg);
         checkbox200kg = (CheckBox) findViewById(R.id.checkbox_200kg);
-        // Q4
+        // Q5
         checkboxInsects = (CheckBox) findViewById(R.id.checkbox_insects);
         checkboxFruitAndVegetables = (CheckBox) findViewById(R.id.checkbox_fruit_vegetables);
         checkboxPlants = (CheckBox) findViewById(R.id.checkbox_plants);
         checkboxFish = (CheckBox) findViewById(R.id.checkbox_fish);
-
-
+        // Q6
+        radioButtonVerySocial = (RadioButton) findViewById(R.id.radio_live_in_groups);
+        radioButtonNotVerySocial = (RadioButton) findViewById(R.id.radio_live_in_pairs);
+        radioButtonNotSocial = (RadioButton) findViewById(R.id.radio_live_alone);
+        // Q7
+        radioButton18 = (RadioButton) findViewById(R.id.radio_18_fingers);
+        radioButton16 = (RadioButton) findViewById(R.id.radio_16_fingers);
+        radioButton20 = (RadioButton) findViewById(R.id.radio_20_fingers);
+        radioButton22 = (RadioButton) findViewById(R.id.radio_22_fingers);
+        // Q8
+        radioButtonLemurYes = (RadioButton) findViewById(R.id.radio_is_lemur);
+        radioButtonLemurNo = (RadioButton) findViewById(R.id.radio_not_lemur);
     }
 
     @Override
@@ -74,38 +97,36 @@ public class MainActivity extends AppCompatActivity {
         score = savedInstanceState.getInt("score");
     }
 
+    public void submitAnswers (View view){
 
-    public void onRadioButtonClicked(View view) {
-        // Is the button now checked?
-        boolean checked = ((RadioButton) view).isChecked();
-
-        switch(view.getId()) {
-            case R.id.radio_madagascar:
-                if (checked)
-                    score++;
-                    break;
-            case R.id.radio_ancestors_no:
-                if (checked)
-                    score++;
-                    break;
-        }
-    }
-
-    public void countScore (View view){
-
+        boolean hasRadioButtonMadagascar = radioButtonMadagascar.isChecked();
+        boolean hasRadioButtonAncestorsNo = radioButtonAncestorsNo.isChecked();
         boolean hasCheckbox30g = checkbox30g.isChecked();
         boolean hasCheckbox9kg = checkbox9kg.isChecked();
+        boolean hasCheckbox50kg = checkbox50kg.isChecked();
         boolean hasCheckbox200kg = checkbox200kg.isChecked();
         boolean hasCheckboxInsects = checkboxInsects.isChecked();
         boolean hasCheckboxFruitAndVegetables = checkboxFruitAndVegetables.isChecked();
         boolean hasCheckboxPlants = checkboxPlants.isChecked();
         boolean hasCheckboxFish = checkboxFish.isChecked();
+        boolean hasRadioButtonVerySocial = radioButtonVerySocial.isChecked();
+        boolean hasRadioButton20 = radioButton20.isChecked();
+        boolean hasRadioButtonLemurYes = radioButtonLemurYes.isChecked();
 
+        if (hasRadioButtonMadagascar){
+            score++;
+        }
+        if (hasRadioButtonAncestorsNo){
+            score++;
+        }
         if (hasCheckbox30g){
             score++;
         }
         if (hasCheckbox9kg){
             score++;
+        }
+        if (hasCheckbox50kg && (score > 0)){
+            score--;
         }
         if (hasCheckbox200kg && (score > 0)){
             score--;
@@ -122,17 +143,27 @@ public class MainActivity extends AppCompatActivity {
         if (hasCheckboxFish && (score > 0)){
             score--;
         }
-        Toast.makeText(this, "Your score is: " + score + "/7", Toast.LENGTH_SHORT).show();
-        displayMessage("Your score is: " + score + "/7");
-        resetScore(view);
+        if (hasRadioButtonVerySocial){
+            score++;
+        }
+        if (hasRadioButton20){
+            score++;
+        }
+        if (hasRadioButtonLemurYes){
+            score++;
+        }
+
+        Toast.makeText(this, "Your score is: " + score + "/10", Toast.LENGTH_SHORT).show();
+        //displayMessage("Your score is: " + score + "/10");
+        //resetScore(view);
     }
 
     public void resetScore (View view){
         score = 0;
 
         // reset radio buttons
-        if (radioButtonGambia.isChecked()){
-            radioButtonGambia.setChecked(false);
+        if (radioButtonVatican.isChecked()){
+            radioButtonVatican.setChecked(false);
         }
         if (radioButtonEurope.isChecked()){
             radioButtonEurope.setChecked(false);
@@ -149,6 +180,33 @@ public class MainActivity extends AppCompatActivity {
         if (radioButtonAncestorsNo.isChecked()){
             radioButtonAncestorsNo.setChecked(false);
         }
+        if (radioButtonVerySocial.isChecked()){
+            radioButtonVerySocial.setChecked(false);
+        }
+        if (radioButtonNotVerySocial.isChecked()){
+            radioButtonNotVerySocial.setChecked(false);
+        }
+        if (radioButtonNotSocial.isChecked()){
+            radioButtonNotSocial.setChecked(false);
+        }
+        if (radioButton18.isChecked()){
+            radioButton18.setChecked(false);
+        }
+        if (radioButton16.isChecked()){
+            radioButton16.setChecked(false);
+        }
+        if (radioButton20.isChecked()){
+            radioButton20.setChecked(false);
+        }
+        if (radioButton22.isChecked()) {
+            radioButton22.setChecked(false);
+        }
+        if (radioButtonLemurYes.isChecked()){
+            radioButtonLemurYes.setChecked(false);
+        }
+        if (radioButtonLemurNo.isChecked()) {
+            radioButtonLemurNo.setChecked(false);
+        }
 
         //reset all checkboxes
         if (checkbox30g.isChecked()) {
@@ -156,6 +214,9 @@ public class MainActivity extends AppCompatActivity {
         }
         if (checkbox9kg.isChecked()) {
             checkbox9kg.setChecked(false);
+        }
+        if (checkbox50kg.isChecked()) {
+            checkbox50kg.setChecked(false);
         }
         if (checkbox200kg.isChecked()) {
             checkbox200kg.setChecked(false);
@@ -173,9 +234,11 @@ public class MainActivity extends AppCompatActivity {
             checkboxFish.setChecked(false);
         }
 
-       // Toast.makeText(this, "Your score is: " + score + "/7", Toast.LENGTH_SHORT).show();
-       // displayMessage("Your score is: " + score + "/7");
+        Toast.makeText(this, "Answers are reset", Toast.LENGTH_SHORT).show();
+       //displayMessage("Answers are reset");
     }
+
+
 
     private void displayMessage(String message) {
         TextView scoreTextView = (TextView) findViewById(R.id.score_text_view);
