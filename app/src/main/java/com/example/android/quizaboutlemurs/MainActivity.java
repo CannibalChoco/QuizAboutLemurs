4170a8d7707e4f4ddfe50b9ea8ac0b1e96bf0574
CannibalChoco <fudgeblimp@gmail.com>
2017-04-04 16:58:54 +0300

    Imageviews moved to onCreate; all strings in strings.xml

--- app/src/main/java/com/example/android/quizaboutlemurs/MainActivity.java
+++ app/src/main/java/com/example/android/quizaboutlemurs/MainActivity.java
@@ -18,55 +18,86 @@ import static com.example.android.quizaboutlemurs.R.string.question8;
 public class MainActivity extends AppCompatActivity {
 
     int score = 0;
-    String rightAnswerEn = "Animal", rightAnswerLv = "Dzīvnieks";
+
+    String rightAnswerEn;
+    String rightAnswerLv;
+
     //Q1
     EditText answer1;
-
     //Q2
     RadioButton radioButtonVatican;
     RadioButton radioButtonEurope;
     RadioButton radioButtonMadagascar;
     RadioButton radioButtonSriLanka;
-
     // Q3
     RadioButton radioButtonMonkeysYes;
     RadioButton radioButtonMonkeysNo;
-
     // Q4
     CheckBox checkbox30g;
     CheckBox checkbox9kg;
     CheckBox checkbox50kg;
     CheckBox checkbox200kg;
-
     // Q5
     CheckBox checkboxInsects;
     CheckBox checkboxFruit;
     CheckBox checkboxPlants;
     CheckBox checkboxFish;
-
     // Q6
     RadioButton radioButtonVerySocial;
     RadioButton radioButtonNotVerySocial;
     RadioButton radioButtonNotSocial;
-
     // Q7
     RadioButton radioButton18;
     RadioButton radioButton16;
     RadioButton radioButton20;
     RadioButton radioButton22;
-
     // Q8
     RadioButton radioButtonLemurYes;
     RadioButton radioButtonLemurNo;
-
+    //Q1
+    ImageView question1;
+    //Q2
+    ImageView question2Answer1;
+    ImageView question2Answer2;
+    ImageView question2Answer3;
+    ImageView question2Answer4;
+    //Q3
+    ImageView question3Answer1;
+    ImageView question3Answer2;
+    //Q4
+    ImageView question4Answer1;
+    ImageView question4Answer2;
+    ImageView question4Answer3;
+    ImageView question4Answer4;
+    //Q5
+    ImageView question5Answer1;
+    ImageView question5Answer2;
+    ImageView question5Answer3;
+    ImageView question5Answer4;
+    //Q6
+    ImageView question6Answer1;
+    ImageView question6Answer2;
+    ImageView question6Answer3;
+    //Q7
+    ImageView question7Answer1;
+    ImageView question7Answer2;
+    ImageView question7Answer3;
+    ImageView question7Answer4;
+    //Q8
+    ImageView question8Answer1;
+    ImageView question8Answer2;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         //Q1
         answer1 = (EditText) findViewById(R.id.edit_text_answer1);
+        rightAnswerLv = getString(R.string.q1_a_lv);
+        rightAnswerEn = getString(R.string.q1_a_en);
+
         // Q2
         radioButtonVatican = (RadioButton) findViewById(R.id.radio_vatican);
         radioButtonEurope = (RadioButton) findViewById(R.id.radio_europe);
@@ -97,6 +128,39 @@ public class MainActivity extends AppCompatActivity {
         // Q8
         radioButtonLemurYes = (RadioButton) findViewById(R.id.radio_is_lemur);
         radioButtonLemurNo = (RadioButton) findViewById(R.id.radio_not_lemur);
+
+        //Q1
+        question1 = (ImageView) findViewById(R.id.image_view_q1);
+        //Q2
+        question2Answer1 = (ImageView) findViewById(R.id.image_view_q2_a1);
+        question2Answer2 = (ImageView) findViewById(R.id.image_view_q2_a2);
+        question2Answer3 = (ImageView) findViewById(R.id.image_view_q2_a3);
+        question2Answer4 = (ImageView) findViewById(R.id.image_view_q2_a4);
+        //Q3
+        question3Answer1 = (ImageView) findViewById(R.id.image_view_q3_a1);
+        question3Answer2 = (ImageView) findViewById(R.id.image_view_q3_a2);
+        //Q4
+        question4Answer1 = (ImageView) findViewById(R.id.image_view_q4_a1);
+        question4Answer2 = (ImageView) findViewById(R.id.image_view_q4_a2);
+        question4Answer3 = (ImageView) findViewById(R.id.image_view_q4_a3);
+        question4Answer4 = (ImageView) findViewById(R.id.image_view_q4_a4);
+        //Q5
+        question5Answer1 = (ImageView) findViewById(R.id.image_view_q5_a1);
+        question5Answer2 = (ImageView) findViewById(R.id.image_view_q5_a2);
+        question5Answer3 = (ImageView) findViewById(R.id.image_view_q5_a3);
+        question5Answer4 = (ImageView) findViewById(R.id.image_view_q5_a4);
+        //Q6
+        question6Answer1 = (ImageView) findViewById(R.id.image_view_q6_a1);
+        question6Answer2 = (ImageView) findViewById(R.id.image_view_q6_a2);
+        question6Answer3 = (ImageView) findViewById(R.id.image_view_q6_a3);
+        //Q7
+        question7Answer1 = (ImageView) findViewById(R.id.image_view_q7_a1);
+        question7Answer2 = (ImageView) findViewById(R.id.image_view_q7_a2);
+        question7Answer3 = (ImageView) findViewById(R.id.image_view_q7_a3);
+        question7Answer4 = (ImageView) findViewById(R.id.image_view_q7_a4);
+        //Q8
+        question8Answer1 = (ImageView) findViewById(R.id.image_view_q8_a1);
+        question8Answer2 = (ImageView) findViewById(R.id.image_view_q8_a2);
     }
 
     @Override
@@ -115,41 +179,8 @@ public class MainActivity extends AppCompatActivity {
      * This method checks the selected answers and sets visibility to corresponding
      * correct/incorrect image
      */
-    public void submitAnswers (View view){
-
-        //Q1
-        ImageView question1 = (ImageView) findViewById(R.id.image_view_q1);
-        //Q2
-        ImageView question2Answer1 = (ImageView) findViewById(R.id.image_view_q2_a1);
-        ImageView question2Answer2 = (ImageView) findViewById(R.id.image_view_q2_a2);
-        ImageView question2Answer3 = (ImageView) findViewById(R.id.image_view_q2_a3);
-        ImageView question2Answer4 = (ImageView) findViewById(R.id.image_view_q2_a4);
-        //Q3
-        ImageView question3Answer1 = (ImageView) findViewById(R.id.image_view_q3_a1);
-        ImageView question3Answer2 = (ImageView) findViewById(R.id.image_view_q3_a2);
-        //Q4
-        ImageView question4Answer1 = (ImageView) findViewById(R.id.image_view_q4_a1);
-        ImageView question4Answer2 = (ImageView) findViewById(R.id.image_view_q4_a2);
-        ImageView question4Answer3 = (ImageView) findViewById(R.id.image_view_q4_a3);
-        ImageView question4Answer4 = (ImageView) findViewById(R.id.image_view_q4_a4);
-        //Q5
-        ImageView question5Answer1 = (ImageView) findViewById(R.id.image_view_q5_a1);
-        ImageView question5Answer2 = (ImageView) findViewById(R.id.image_view_q5_a2);
-        ImageView question5Answer3 = (ImageView) findViewById(R.id.image_view_q5_a3);
-        ImageView question5Answer4 = (ImageView) findViewById(R.id.image_view_q5_a4);
-        //Q6
-        ImageView question6Answer1 = (ImageView) findViewById(R.id.image_view_q6_a1);
-        ImageView question6Answer2 = (ImageView) findViewById(R.id.image_view_q6_a2);
-        ImageView question6Answer3 = (ImageView) findViewById(R.id.image_view_q6_a3);
-        //Q7
-        ImageView question7Answer1 = (ImageView) findViewById(R.id.image_view_q7_a1);
-        ImageView question7Answer2 = (ImageView) findViewById(R.id.image_view_q7_a2);
-        ImageView question7Answer3 = (ImageView) findViewById(R.id.image_view_q7_a3);
-        ImageView question7Answer4 = (ImageView) findViewById(R.id.image_view_q7_a4);
-        //Q8
-        ImageView question8Answer1 = (ImageView) findViewById(R.id.image_view_q8_a1);
-        ImageView question8Answer2 = (ImageView) findViewById(R.id.image_view_q8_a2);
-
+    public void submitAnswers(View view) {
+        score = 0;
 
         // Q2
         boolean hasRadioButtonVatican = radioButtonVatican.isChecked();
@@ -189,182 +220,148 @@ public class MainActivity extends AppCompatActivity {
         boolean hasRadioButtonLemurNo = radioButtonLemurNo.isChecked();
 
         //Q1
-        if ((answer1.getText().toString().equals(rightAnswerEn)) || (answer1.getText().toString().equals(rightAnswerLv))){
+        if ((answer1.getText().toString().equals(rightAnswerEn)) || (answer1.getText().toString().equals(rightAnswerLv))) {
             score++;
             question1.setImageResource(R.drawable.correct);
             question1.setVisibility(View.VISIBLE);
-        }
-        else{
+        } else {
             question1.setImageResource(R.drawable.incorrect);
             question1.setVisibility(View.VISIBLE);
         }
         //Q2
-        if (hasRadioButtonVatican){
+        if (hasRadioButtonVatican) {
             question2Answer1.setVisibility(View.VISIBLE);
         }
-        if (hasRadioButtonEurope){
+        if (hasRadioButtonEurope) {
             question2Answer2.setVisibility(View.VISIBLE);
         }
-        if (hasRadioButtonMadagascar){
+        if (hasRadioButtonMadagascar) {
             score++;
             question2Answer3.setVisibility(View.VISIBLE);
         }
-        if (hasRadioButtonSriLanka){
+        if (hasRadioButtonSriLanka) {
             question2Answer4.setVisibility(View.VISIBLE);
         }
         //Q3
-        if (hasRadioButtonMonkeysYes){
+        if (hasRadioButtonMonkeysYes) {
             question3Answer1.setVisibility(View.VISIBLE);
         }
-        if (hasRadioButtonMonkeysNo){
+        if (hasRadioButtonMonkeysNo) {
             score++;
             question3Answer2.setVisibility(View.VISIBLE);
         }
         //Q4
-        if (hasCheckbox30g){
+        if (hasCheckbox30g) {
             score++;
             question4Answer1.setVisibility(View.VISIBLE);
         }
-        if (hasCheckbox9kg){
+        if (hasCheckbox9kg) {
             score++;
             question4Answer2.setVisibility(View.VISIBLE);
         }
-        if (hasCheckbox50kg){
-            if (score > 0){
+        if (hasCheckbox50kg) {
+            if (score > 0) {
                 score--;
             }
             question4Answer3.setVisibility(View.VISIBLE);
         }
-        if (hasCheckbox200kg ){
-            if (score > 0){
+        if (hasCheckbox200kg) {
+            if (score > 0) {
                 score--;
             }
             question4Answer4.setVisibility(View.VISIBLE);
         }
         //Q5
-        if (hasCheckboxInsects){
+        if (hasCheckboxInsects) {
             score++;
             question5Answer1.setVisibility(View.VISIBLE);
         }
-        if (hasCheckboxFruit){
+        if (hasCheckboxFruit) {
             score++;
             question5Answer2.setVisibility(View.VISIBLE);
         }
-        if (hasCheckboxPlants){
+        if (hasCheckboxPlants) {
             score++;
             question5Answer3.setVisibility(View.VISIBLE);
         }
-        if (hasCheckboxFish){
-            if(score > 0){
+        if (hasCheckboxFish) {
+            if (score > 0) {
                 score--;
             }
             question5Answer4.setVisibility(View.VISIBLE);
         }
         //Q6
-        if (hasRadioButtonVerySocial){
+        if (hasRadioButtonVerySocial) {
             score++;
             question6Answer1.setVisibility(View.VISIBLE);
         }
-        if (hasRadioButtonNotVerySocial){
+        if (hasRadioButtonNotVerySocial) {
             question6Answer2.setVisibility(View.VISIBLE);
         }
-        if (hasRadioButtonNotSocial){
+        if (hasRadioButtonNotSocial) {
             question6Answer3.setVisibility(View.VISIBLE);
         }
         //Q7
-        if (hasRadioButton18){
+        if (hasRadioButton18) {
             question7Answer1.setVisibility(View.VISIBLE);
         }
-        if (hasRadioButton16){
+        if (hasRadioButton16) {
             question7Answer2.setVisibility(View.VISIBLE);
         }
-        if (hasRadioButton20){
+        if (hasRadioButton20) {
             score++;
             question7Answer3.setVisibility(View.VISIBLE);
         }
-        if (hasRadioButton22){
+        if (hasRadioButton22) {
             question7Answer4.setVisibility(View.VISIBLE);
         }
         //Q8
-        if (hasRadioButtonLemurYes){
+        if (hasRadioButtonLemurYes) {
             score++;
             question8Answer1.setVisibility(View.VISIBLE);
         }
-        if (hasRadioButtonLemurNo){
+        if (hasRadioButtonLemurNo) {
             question8Answer2.setVisibility(View.VISIBLE);
         }
 
-        Toast.makeText(this, "Your score is: " + score + "/11", Toast.LENGTH_SHORT).show();
-
+        String message1 = getString(R.string.toast_submit1);
+        String message2 = getString(R.string.toast_submit2);
+        Toast.makeText(this, message1 + " " + score + message2, Toast.LENGTH_SHORT).show();
     }
 
     /**
      * Method to clear answers. It also sets the correct/incorrect images
      * to invisible
      */
-    public void resetAll (View view){
+    public void resetAll(View view) {
         score = 0;
-
-        //Q1
-        ImageView question1 = (ImageView) findViewById(R.id.image_view_q1);
-        //Q2
-        ImageView question2Answer1 = (ImageView) findViewById(R.id.image_view_q2_a1);
-        ImageView question2Answer2 = (ImageView) findViewById(R.id.image_view_q2_a2);
-        ImageView question2Answer3 = (ImageView) findViewById(R.id.image_view_q2_a3);
-        ImageView question2Answer4 = (ImageView) findViewById(R.id.image_view_q2_a4);
-        //Q3
-        ImageView question3Answer1 = (ImageView) findViewById(R.id.image_view_q3_a1);
-        ImageView question3Answer2 = (ImageView) findViewById(R.id.image_view_q3_a2);
-        //Q4
-        ImageView question4Answer1 = (ImageView) findViewById(R.id.image_view_q4_a1);
-        ImageView question4Answer2 = (ImageView) findViewById(R.id.image_view_q4_a2);
-        ImageView question4Answer3 = (ImageView) findViewById(R.id.image_view_q4_a3);
-        ImageView question4Answer4 = (ImageView) findViewById(R.id.image_view_q4_a4);
-        //Q5
-        ImageView question5Answer1 = (ImageView) findViewById(R.id.image_view_q5_a1);
-        ImageView question5Answer2 = (ImageView) findViewById(R.id.image_view_q5_a2);
-        ImageView question5Answer3 = (ImageView) findViewById(R.id.image_view_q5_a3);
-        ImageView question5Answer4 = (ImageView) findViewById(R.id.image_view_q5_a4);
-        //Q6
-        ImageView question6Answer1 = (ImageView) findViewById(R.id.image_view_q6_a1);
-        ImageView question6Answer2 = (ImageView) findViewById(R.id.image_view_q6_a2);
-        ImageView question6Answer3 = (ImageView) findViewById(R.id.image_view_q6_a3);
-        //Q7
-        ImageView question7Answer1 = (ImageView) findViewById(R.id.image_view_q7_a1);
-        ImageView question7Answer2 = (ImageView) findViewById(R.id.image_view_q7_a2);
-        ImageView question7Answer3 = (ImageView) findViewById(R.id.image_view_q7_a3);
-        ImageView question7Answer4 = (ImageView) findViewById(R.id.image_view_q7_a4);
-        //Q8
-        ImageView question8Answer1 = (ImageView) findViewById(R.id.image_view_q8_a1);
-        ImageView question8Answer2 = (ImageView) findViewById(R.id.image_view_q8_a2);
-
 
         //Q1
         answer1.setText("");
         question1.setVisibility(View.INVISIBLE);
         //Q2
-        if (radioButtonVatican.isChecked()){
+        if (radioButtonVatican.isChecked()) {
             radioButtonVatican.setChecked(false);
             question2Answer1.setVisibility(View.INVISIBLE);
         }
-        if (radioButtonEurope.isChecked()){
+        if (radioButtonEurope.isChecked()) {
             radioButtonEurope.setChecked(false);
             question2Answer2.setVisibility(View.INVISIBLE);
         }
-        if (radioButtonMadagascar.isChecked()){
+        if (radioButtonMadagascar.isChecked()) {
             radioButtonMadagascar.setChecked(false);
             question2Answer3.setVisibility(View.INVISIBLE);
         }
-        if (radioButtonSriLanka.isChecked()){
+        if (radioButtonSriLanka.isChecked()) {
             radioButtonSriLanka.setChecked(false);
             question2Answer4.setVisibility(View.INVISIBLE);
         }
         //Q3
-        if (radioButtonMonkeysYes.isChecked()){
+        if (radioButtonMonkeysYes.isChecked()) {
             radioButtonMonkeysYes.setChecked(false);
             question3Answer1.setVisibility(View.INVISIBLE);
         }
-        if (radioButtonMonkeysNo.isChecked()){
+        if (radioButtonMonkeysNo.isChecked()) {
             radioButtonMonkeysNo.setChecked(false);
             question3Answer2.setVisibility(View.INVISIBLE);
         }
@@ -403,28 +400,28 @@ public class MainActivity extends AppCompatActivity {
             question5Answer4.setVisibility(View.INVISIBLE);
         }
         //Q6
-        if (radioButtonVerySocial.isChecked()){
+        if (radioButtonVerySocial.isChecked()) {
             radioButtonVerySocial.setChecked(false);
             question6Answer1.setVisibility(View.INVISIBLE);
         }
-        if (radioButtonNotVerySocial.isChecked()){
+        if (radioButtonNotVerySocial.isChecked()) {
             radioButtonNotVerySocial.setChecked(false);
             question6Answer2.setVisibility(View.INVISIBLE);
         }
-        if (radioButtonNotSocial.isChecked()){
+        if (radioButtonNotSocial.isChecked()) {
             radioButtonNotSocial.setChecked(false);
             question6Answer3.setVisibility(View.INVISIBLE);
         }
         //Q7
-        if (radioButton18.isChecked()){
+        if (radioButton18.isChecked()) {
             radioButton18.setChecked(false);
             question7Answer1.setVisibility(View.INVISIBLE);
         }
-        if (radioButton16.isChecked()){
+        if (radioButton16.isChecked()) {
             radioButton16.setChecked(false);
             question7Answer2.setVisibility(View.INVISIBLE);
         }
-        if (radioButton20.isChecked()){
+        if (radioButton20.isChecked()) {
             radioButton20.setChecked(false);
             question7Answer3.setVisibility(View.INVISIBLE);
         }
@@ -432,7 +429,7 @@ public class MainActivity extends AppCompatActivity {
             radioButton22.setChecked(false);
             question7Answer4.setVisibility(View.INVISIBLE);
         }
-        if (radioButtonLemurYes.isChecked()){
+        if (radioButtonLemurYes.isChecked()) {
             radioButtonLemurYes.setChecked(false);
             question8Answer1.setVisibility(View.INVISIBLE);
         }
@@ -441,7 +438,8 @@ public class MainActivity extends AppCompatActivity {
             question8Answer2.setVisibility(View.INVISIBLE);
         }
 
-        Toast.makeText(this, "Answers are reset", Toast.LENGTH_SHORT).show();
+        String message = getString(R.string.toast_reset);
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 
 /*
